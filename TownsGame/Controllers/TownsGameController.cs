ab8df23bd418bbd298e8de0ded84061cea1fae80
Teremok007 <Nthtvjr007>
2018-11-05 00:15:29 +0200

    create empty controller and view

---
+++ TownsGame/Controllers/TownsGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TownsGame.Controllers
+{
+    public class TownsGameController : Controller
+    {
+        // GET: TownsGame
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TownsGame/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TownsGame.MvcApplication" Language="C#" %>
