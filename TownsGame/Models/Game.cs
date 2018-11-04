c5673d0faebd6f0c597343d420495a4071a6ea33
Teremok007 <Nthtvjr007>
2018-11-05 00:55:33 +0200

    create StepGame object

---
+++ TownsGame/Models/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TownsGame.Models
+{
+    public static class Game
+    {
+        private static HashSet<string> allTowns = new HashSet<string>();
+        private static HashSet<string> usedTowns = new HashSet<string>();
+
+        static Game()
+        {
+            using (StreamReader readerTown = new StreamReader(new FileInfo("TownList.txt").OpenRead()))
+            {
+                while (!readerTown.EndOfStream)
+                {
+                    allTowns.Add(readerTown.ReadLine());
+                }
+            }
+        }
+
+        public static void AddStep(string Town)
+        {
+
+        }
+
+    }
+}
---
+++ TownsGame/Models/StepGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TownsGame.Models
+{
+    public class StepGame
+    {
+        public string Town { get; set; }
+
+        public string GamerName { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
